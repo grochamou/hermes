6c09e8a275f2c615167be7516b7578eef9899476
Martin Hou <grochamou@gmail.com>
2022-08-28 16:20:52 +0200

    Updated POM. Removed Heroku support.

--- build-heroku.sh
+++
@@ -1,1 +0,0 @@
-./mvnw clean install -Pheroku
--- deploy-heroku.sh
+++
@@ -1,4 +0,0 @@
-heroku apps:destroy --app gcm-hermes-server --confirm gcm-hermes-server
-heroku apps:create --region eu gcm-hermes-server
-heroku config:set --app gcm-hermes-server SPRING_PROFILES_ACTIVE=heroku
-./mvnw heroku:deploy-war -f hermes-server/pom.xml -Pheroku
